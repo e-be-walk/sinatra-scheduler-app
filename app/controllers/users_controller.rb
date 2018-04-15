6348176aff284f3c58febc7ea88cc6ddf7655415
Erin Walker <erinb0317@hotmail.com>
2018-04-15 15:47:54 +0000

    Users controllers and view updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,5 +19,31 @@ class UsersController < ApplicationController
       redirect '/signup'
     end
   end
-  
+
+  get '/login' do
+    if logged_in?
+      redirect '/scheduler'
+    end
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(params[:user])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect '/scheduler'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end 
 end
