6b74a25ff9d1986c11816fb73dd710afe01391fe
Erin Walker <erinb0317@hotmail.com>
2018-05-07 21:56:02 +0000

    Fixing a mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
   has_many :appointments
   has_many :todos
   validates :username, :email, :password, presence: true
-  validates :username, :email, uniqueness: true 
+  validates :username, :email, uniqueness: true
+
+  def sorted_appointments
+    Appointment.sort_by {|appointment| appointment.datetime}
+  end
 end
